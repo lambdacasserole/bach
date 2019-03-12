96533126430bb489a4dc265ceba718c2a323e78a
Saul Johnson <saul.a.johnson@gmail.com>
2019-03-12 12:56:42 +0000

    Document ConnectionCapacity.java

--- src/main/java/com/sauljohnson/bach/ConnectionCapacity.java
+++ src/main/java/com/sauljohnson/bach/ConnectionCapacity.java
@@ -1,19 +1,47 @@
 package com.sauljohnson.bach;
 
+/**
+ * Represents a connection capacity.
+ *
+ * @author Saul Johnson
+ */
 public class ConnectionCapacity {
+
+    /**
+     * The connection capacity (i.e. the maximum number of simultaneous connection).
+     */
     private int count;
 
+    /**
+     * The connection type.
+     */
     private Class type;
 
+    /**
+     * Initialises a new instance of a connection capacity.
+     *
+     * @param count the number number of simultaneous connections
+     * @param type  the connection type
+     */
     public ConnectionCapacity(int count, Class type) {
         this.count = count;
         this.type = type;
     }
 
+    /**
+     * Gets the connection capacity (i.e. the maximum number of simultaneous connection).
+     *
+     * @return  the connection capacity (i.e. the maximum number of simultaneous connection)
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     * Gets the connection type.
+     *
+     * @return  the connection type
+     */
     public Class getType() {
         return type;
     }
